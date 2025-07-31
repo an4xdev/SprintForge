6bfebc1b8653c8ec00220c59fd63fd5d8aad52e6
Michał Żuk <michal.zuk30601@gmail.com>
2025-07-31 19:50:15 +0200

    passed test spring teams scenario

---
+++ ApiGateway/ApiGateway/Models/CreateTeamDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiGateway.Models;
+
+public class CreateTeamDto
+{
+    [JsonPropertyName("ManagerId")]
+    public Guid? ManagerId { get; set; }
+    [JsonPropertyName("Name")]
+    public string? Name { get; set; }
+}
---
+++ ApiGateway/ApiGateway/Models/TeamDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiGateway.Models;
+
+public class TeamDto
+{
+    [JsonPropertyName("Id")]
+    public string Id { get; set; }
+    [JsonPropertyName("Name")]
+    public string Name { get; set; }
+    [JsonPropertyName("Manager")]
+    public ManagerDto Manager { get; set; }
+
+}
+public class ManagerDto
+{
+    [JsonPropertyName("Id")]
+    public string Id { get; set; }
+    [JsonPropertyName("Username")]
+    public string Username { get; set; }
+}
