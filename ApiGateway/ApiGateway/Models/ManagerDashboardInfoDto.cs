e7ae978a69b7ab3a396b83e029c9fd34f2c738f1
Michał Żuk <michal.zuk30601@gmail.com>
2025-08-15 21:30:25 +0200

    feat: .

--- ApiGateway/ApiGateway/Models/ManagerDashboardInfoDto.cs
+++ ApiGateway/ApiGateway/Models/ManagerDashboardInfoDto.cs
@@ -4,15 +4,8 @@ namespace ApiGateway.Models;
 
 public class ManagerDashboardInfoDto
 {
-    [JsonPropertyName("sprints")] public List<ManagerSprintDto> Sprints { get; set; }
-    [JsonPropertyName("isActive")] public bool IsActive { get; set; }
-    [JsonPropertyName("tasksBySprint")] public List<ManagerTasksBySprintDto> TasksBySprint { get; set; }
-}
-
-public class ManagerTasksBySprintDto
-{
-    [JsonPropertyName("sprintId")] public Guid SprintId { get; set; }
-    [JsonPropertyName("tasks")] public List<ManagerTaskDto> Tasks { get; set; }
+    [JsonPropertyName("sprint")] public ManagerSprintDto? Sprint { get; set; }
+    [JsonPropertyName("tasks")] public List<ManagerTaskDto> Tasks { get; set; } = [];
 }
 
 public class ManagerSprintDto
@@ -20,6 +13,12 @@ public class ManagerSprintDto
     [JsonPropertyName("id")] public Guid SprintId { get; set; }
 
     [JsonPropertyName("name")] public string Name { get; set; }
+
+    [JsonPropertyName("active")] public bool IsActive { get; set; }
+
+    [JsonPropertyName("startDate")] public DateOnly StartDate { get; set; }
+
+    [JsonPropertyName("endDate")] public DateOnly EndDate { get; set; }
 }
 
 public class ManagerTaskDto
